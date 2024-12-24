652480a9931de1795f9446e6088fb69d009593cb
Abdullah Ghaith <abdullahghiath2003@gmail.com>
2024-12-24 18:02:15 +0200

    - added url launcher package to handle calls between apps - changed app drawer layout

--- lib/core/widgets/custom_appdrawer.dart
+++ lib/core/widgets/custom_appdrawer.dart
@@ -7,11 +7,35 @@ import 'package:atef_physics/features/profile/screens/profile_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomAppdrawer extends StatelessWidget {
   const CustomAppdrawer({super.key});
   @override
   Widget build(BuildContext context) {
+    void _launchWhatsApp(String phoneNumber) async {
+      final Uri whatsappUri = Uri.parse("https://wa.me/$phoneNumber");
+
+      if (await canLaunchUrl(whatsappUri)) {
+        await launchUrl(whatsappUri, mode: LaunchMode.externalApplication);
+      } else {
+        throw 'Could not launch WhatsApp for $phoneNumber';
+      }
+    }
+
+    void _callPhoneNumber(String phoneNumber) async {
+      final Uri phoneUri = Uri(scheme: 'tel', path: phoneNumber);
+      try {
+        if (await canLaunchUrl(phoneUri)) {
+          await launchUrl(phoneUri, mode: LaunchMode.externalApplication);
+        } else {
+          throw 'Could not launch $phoneNumber';
+        }
+      } catch (e) {
+        debugPrint('Error: $e');
+      }
+    }
+
     final UserModel user = Storage.instance.user;
     return Drawer(
       child: Padding(
@@ -22,38 +46,10 @@ class CustomAppdrawer extends StatelessWidget {
               height: MediaQuery.viewPaddingOf(context).top + 20,
             ),
             Text(
-              " ${user.name} مرحبا بك",
+              " ${user.name}",
               style: AppTextStyles.hevoLight20BlackWhiteW900,
             ),
             const SizedBox(height: 30),
-            Text(
-              "Admin User",
-              style: AppTextStyles.hevoLight20BlackWhiteW900,
-            ),
-            const Divider(),
-            const SizedBox(height: 10),
-            GestureDetector(
-                onTap: () {
-                  context.pushNamed(ProfileScreen.id);
-                },
-                child: const Text('الوحده الاولي')),
-            const SizedBox(height: 10),
-            const Divider(),
-            const SizedBox(height: 10),
-            GestureDetector(
-                onTap: () {
-                  context.pushNamed(ProfileScreen.id);
-                },
-                child: const Text('الوحده التانيه')),
-            const SizedBox(height: 10),
-            const Divider(),
-            const SizedBox(height: 10),
-            GestureDetector(
-                onTap: () {
-                  context.pushNamed(ProfileScreen.id);
-                },
-                child: const Text('الوحده التالته')),
-            const SizedBox(height: 10),
             const Divider(),
             const SizedBox(height: 10),
             GestureDetector(
@@ -69,26 +65,58 @@ class CustomAppdrawer extends StatelessWidget {
             const SizedBox(height: 10),
             const Divider(),
             const Spacer(),
+            const Divider(),
+            const SizedBox(height: 10),
             GestureDetector(
                 onTap: () {
-                  FirebaseAuth.instance.signOut();
-                  context.pushReplacementNamed(LoginScreen.id);
+                  _callPhoneNumber('201018964256');
                 },
-                child: IconButton(
-                    onPressed: () async {
-                      await FirebaseAuth.instance.signOut();
-                      await Storage.instance.logout();
-                      if (context.mounted) {
-                        context.pushReplacementNamed(LoginScreen.id);
-                      }
-                    },
-                    icon: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.logout_rounded),
-                          SizedBox(width: 10),
-                          Text('تسجيل الخروج')
-                        ])))
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(width: 10),
+                    Text('تواصل معنا عبر الهاتف ')
+                  ],
+                )),
+            const SizedBox(height: 10),
+            const Divider(),
+            const SizedBox(height: 10),
+            GestureDetector(
+                onTap: () {
+                  _launchWhatsApp('201018964256');
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.message,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(width: 10),
+                    Text('تواصل معنا عبر الواتس ')
+                  ],
+                )),
+            const SizedBox(height: 10),
+            const Divider(),
+            IconButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  await Storage.instance.logout();
+                  if (context.mounted) {
+                    context.pushReplacementNamed(LoginScreen.id);
+                  }
+                },
+                icon: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.logout_rounded),
+                      SizedBox(width: 10),
+                      Text('تسجيل الخروج')
+                    ]))
           ],
         ),
       ),
