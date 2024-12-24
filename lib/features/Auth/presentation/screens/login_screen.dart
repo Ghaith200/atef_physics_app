e2c38feed8a69d9023794794ad7ddb15b7596b2c
Abdullah Ghaith <abdullahghiath2003@gmail.com>
2024-12-24 14:33:53 +0200

    - fixed login button loading - changed the layout of the login screen - removed unused pacakges

--- lib/features/Auth/presentation/screens/login_screen.dart
+++ lib/features/Auth/presentation/screens/login_screen.dart
@@ -45,115 +45,128 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.sp, vertical: 50.sp),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Assets.images.logoPng.image(
-              //   width: 200.sp,
-              //   height: 200.sp,
-              //   // fit: BoxFit.contain,
-              // ),
-              const SizedBox(height: 20),
-              Text(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.sp, vertical: 20.sp),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Assets.images.logoPng.image(
+            //   width: 200.sp,
+            //   height: 200.sp,
+            //   // fit: BoxFit.contain,
+            // ),
+            const Spacer(),
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
                 'Welcome Back',
                 style: AppTextStyles.hevoLight25WhiteBlackW900,
               ),
-              SizedBox(height: 50.sp),
+            ),
 
-              Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      CustomTextFormField(
-                          textEditingController: emailcon,
-                          validator: (p0) => Validators.emailValidator(p0!),
-                          keyboardType: TextInputType.emailAddress,
-                          inputTextStyle:
-                              TextStyle(fontSize: 10.sp, color: Colors.white),
-                          hintText: 'Enter your email',
-                          prefixIcon: Assets.images.icons.emailIcon
-                              .image(color: AppColors.whiteBlack),
-                          label: 'Email'),
-
-                      SizedBox(height: 10.sp),
-
-                      // Password Text Form Field
-                      CustomTextFormField(
-                          validator: (p0) => Validators.passwordValidator(p0),
-                          textEditingController: passcon,
-                          suffixIconConstraints:
-                              const BoxConstraints(minWidth: 50, minHeight: 50),
-                          hintText: 'Enter your password',
-                          obscureText: true,
-                          prefixIcon: Assets.images.icons.lockIcon
-                              .image(color: AppColors.whiteBlack),
-                          label: 'Password'),
-                      SizedBox(height: 20.sp),
-                    ],
-                  )),
-              // Email Text Form Field
-
-              //Login Button
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.sp),
-                child: BlocConsumer<AuthCubit, AuthState>(
-                  listener: (context, state) => state.whenOrNull(
-                    error: (error) => error.showError(context),
-                    success: (userModel) {
-                      AppSnackBar.showSnackBar(
-                          context, "welcome ${userModel.name}");
-                      context.pushNamed(HomeScreen.id);
-                      return null;
-                    },
+            const Spacer(),
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    textEditingController: emailcon,
+                    validator: (p0) => Validators.emailValidator(p0!),
+                    keyboardType: TextInputType.emailAddress,
+                    inputTextStyle:
+                        TextStyle(fontSize: 10.sp, color: Colors.white),
+                    hintText: 'Enter your email',
+                    prefixIcon: Assets.images.icons.emailIcon
+                        .image(color: AppColors.whiteBlack),
+                    label: 'Email',
                   ),
-                  builder: (context, state) {
-                    state.whenOrNull(
-                      loading: () => CustomButton(
-                        onTap: () async {},
-                        filled: true,
-                        boarderRadius: 25.sp,
-                        // title: 'Login',
-                        child: const CircularProgressIndicator(),
-                      ),
-                    );
-                    return CustomButton(
-                      onTap: () async =>
-                          (formKey.currentState?.validate() ?? false)
-                              ? await authCubit.login(
-                                  email: emailcon.text.trim(),
-                                  pass: passcon.text.trim())
-                              : null,
-                      filled: true,
-                      boarderRadius: 25.sp,
-                      title: 'Login',
-                    );
+                  SizedBox(height: 10.sp),
+
+                  // Password Text Form Field
+                  CustomTextFormField(
+                    validator: (p0) => Validators.passwordValidator(p0),
+                    textEditingController: passcon,
+                    suffixIconConstraints:
+                        const BoxConstraints(minWidth: 50, minHeight: 50),
+                    hintText: 'Enter your password',
+                    obscureText: true,
+                    prefixIcon: Assets.images.icons.lockIcon
+                        .image(color: AppColors.whiteBlack),
+                    label: 'Password',
+                  ),
+                  SizedBox(height: 20.sp),
+                ],
+              ),
+            ),
+            const Spacer(),
+            // Login Button
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.sp),
+              child: BlocConsumer<AuthCubit, AuthState>(
+                listener: (context, state) => state.whenOrNull(
+                  error: (error) => error.showError(context),
+                  success: (userModel) {
+                    AppSnackBar.showSnackBar(
+                        context, "Welcome ${userModel.name}");
+                    context.pushNamed(HomeScreen.id);
+                    return null;
                   },
                 ),
-              ),
-              const Spacer(),
-              SizedBox(height: 20.sp),
-              GestureDetector(
-                onTap: () {
-                  context.push(SignUpScreen.id);
+                builder: (context, state) {
+                  if (state is AuthLoading) {
+                    return CustomButton(
+                      height: 55,
+                      onTap: () async {},
+                      filled: true,
+                      boarderRadius: 25.sp,
+                      child: const Center(
+                          child: CircularProgressIndicator(
+                        color: Color(0xff082a3a),
+                      )),
+                    );
+                  }
+
+                  return CustomButton(
+                    height: 55,
+                    onTap: () async =>
+                        (formKey.currentState?.validate() ?? false)
+                            ? await authCubit.login(
+                                email: emailcon.text.trim(),
+                                pass: passcon.text.trim(),
+                              )
+                            : null,
+                    filled: true,
+                    boarderRadius: 25.sp,
+                    title: 'Login',
+                  );
                 },
-                child: Text.rich(TextSpan(children: [
-                  TextSpan(
-                      text: "Don't have an account ?  ",
-                      style: AppTextStyles.hevoLight15WhiteBlackW500),
-                  TextSpan(
-                    text: "Sign Up",
-                    style: AppTextStyles.hevoLight15WhiteBlackW500.copyWith(
-                      decoration: TextDecoration.underline,
-                      decorationColor: AppColors.whiteBlack,
-                    ),
-                  )
-                ])),
               ),
-            ],
-          ),
-        ));
+            ),
+
+            SizedBox(height: 50.sp),
+            GestureDetector(
+              onTap: () {
+                context.push(SignUpScreen.id);
+              },
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "Don't have an account?  ",
+                  style: AppTextStyles.hevoLight15WhiteBlackW500,
+                ),
+                TextSpan(
+                  text: "Sign Up",
+                  style: AppTextStyles.hevoLight15WhiteBlackW500.copyWith(
+                    decoration: TextDecoration.underline,
+                    decorationColor: AppColors.whiteBlack,
+                  ),
+                ),
+              ])),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
